55d2017cce3d232b41fe76aa8a6ae1580344f4fa
TheAresius <mbandeira.fsb@gmail.com>
2022-10-03 19:56:15 -0300

    Calculation history update

---
+++ strings.js
@@ -0,0 +1,29 @@
+// file to make translation easier
+
+const stringsAlertHarkyon =
+  "Nota: As pastas do Harkyon ignoram o buff de dano do título.\nCaso possua um título com buff de dano lembre-se de remover 5% ou 10% na opção 'Buffs'.";
+const stringsErrorInvalidData =
+  "Algo de errado não está certo. Reveja os valores inseridos.";
+const stringsErrorHarrier =
+  "Você não pode aplicar o Debuff de Harrier neste monstro em específico. Reveja os valores inseridos.";
+
+const stringsComboDamage = "Dano de combo";
+const stringsSkillDamage = "Dano de skill";
+const stringsPetDamage = "Dano de pet";
+
+// -- historyListStrings
+const stringsHistoryListTitle = "Histórico de Cálculos";
+const stringsHistoryListCharacterLevel = "Nível do personagem";
+const stringsHistoryListMonsterLevel = "Nível do monstro";
+const stringsHistoryListAttack = "Ataque";
+const stringsHistoryListSpecialAttack = "Ataque especial";
+const stringHistoryListCriticalRate = "Chance de crítico";
+const stringHistoryListCriticalDamage = "Dano crítico";
+const stringsHistoryListHarrierDebuff = "Debuff de Harrier";
+const stringsHistoryListHarrierResistance = "Resistência a Harrier";
+const stringsHistoryListBuffs = "Buffs";
+const stringsHistoryListDamageValue = "Valor do dano";
+
+const stringsNormalHitDamage = "Hit normal";
+const stringsCriticalHitDamage = "Hit crítico";
+const stringsAverageDamage = "Dano médio";
