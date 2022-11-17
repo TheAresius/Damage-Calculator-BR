bf1102942b930de2d1ccd8cfa43826ce6070907b
TheAresius <mbandeira.fsb@gmail.com>
2022-11-17 14:27:34 -0300

    Harrier/Taint name change

--- strings.js
+++ strings.js
@@ -5,7 +5,7 @@ const stringsAlertHarkyon =
 const stringsErrorInvalidData =
   "Algo de errado não está certo. Reveja os valores inseridos.";
 const stringsErrorHarrier =
-  "Você não pode aplicar o Debuff de Harrier neste monstro em específico. Reveja os valores inseridos.";
+  "Você não pode aplicar o Debuff de Contaminação neste monstro em específico. Reveja os valores inseridos.";
 
 const stringsComboDamage = "Dano de combo";
 const stringsSkillDamage = "Dano de skill";
@@ -19,8 +19,8 @@ const stringsHistoryListAttack = "Ataque";
 const stringsHistoryListSpecialAttack = "Ataque especial";
 const stringHistoryListCriticalRate = "Chance de crítico";
 const stringHistoryListCriticalDamage = "Dano crítico";
-const stringsHistoryListHarrierDebuff = "Debuff de Harrier";
-const stringsHistoryListHarrierResistance = "Resistência a Harrier";
+const stringsHistoryListHarrierDebuff = "Debuff Contaminação";
+const stringsHistoryListHarrierResistance = "Resist. Contaminação";
 const stringsHistoryListBuffs = "Buffs";
 const stringsHistoryListDamageValue = "Valor do dano";
 
