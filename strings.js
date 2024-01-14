9f4879581db485c7320878d532cc53ca6a33b1b8
TheAresius <mbandeira.fsb@gmail.com>
2024-01-14 02:22:42 -0300

    v1.3.1
    void taint added

--- strings.js
+++ strings.js
@@ -8,7 +8,8 @@ const stringsErrorHarrier =
   "Você não pode aplicar o Debuff de Contaminação neste monstro em específico. Reveja os valores inseridos.";
 const stringsAlertRaid = 
   "Nota: O valor de 20% de debuff de contaminação está sendo aplicado por conta da raid. Não esqueça de inserir sua resistência a contaminação."
-
+const stringsAlertVoid2 = 
+  "Nota: O buff adquirido do Executor do Vazio, presente nas partes 2, 3 e 4 do Vazio (Contaminação) é de 200%. Some esse valor aos seus buffs, caso deseje."
 const stringsComboDamage = "Dano de combo";
 const stringsSkillDamage = "Dano de skill";
 const stringsPetDamage = "Dano de pet";
